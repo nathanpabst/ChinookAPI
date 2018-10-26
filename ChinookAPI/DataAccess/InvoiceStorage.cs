25af93eba0696c486bdbfcb94b42ff7caf3fb2f7
Nathan Pabst <nathan.pabst@gmail.com>
2018-10-26 18:56:15 -0500

    finished adding fields for the second deliverable

--- ChinookAPI/DataAccess/InvoiceStorage.cs
+++ ChinookAPI/DataAccess/InvoiceStorage.cs
@@ -17,7 +17,7 @@ namespace ChinookAPI.DataAccess
         private const string ConnectionString = @"Server=.\SQLEXPRESS;Database=Chinook;Trusted_Connection=True;";
 
 
-        //Provide an endpoint that shows the Invoice Total, Customer name, Country and Sale Agent name for all invoices.
+        //2) Provide an endpoint that shows the Invoice Total, Customer name, Country and Sale Agent name for all invoices.
         public List<Invoice> GetInvoices()
         {
             using (var connection = new SqlConnection(ConnectionString))
@@ -51,17 +51,18 @@ namespace ChinookAPI.DataAccess
             }
         }
 
-        //Provide an endpoint that shows the Invoice Total, Customer name, Country and Sale Agent name for all invoices.
+        //1) Provide an endpoint that shows the invoices associated with each sales agent. The result should include the Sales Agent's full name.
         public List<SalesAgent> GetInvoicesByRep()
         {
+
             using (var connection = new SqlConnection(ConnectionString))
             {
                 connection.Open();
 
                 var command = connection.CreateCommand();
-                command.CommandText = @"SELECT 
+                command.CommandText = @"SELECT                                
 	                                        [Agent Name] = e.FirstName + ' ' + e.LastName,
-	                                        [Invoice Id] = i.InvoiceId
+                                            i.*
                                         FROM Employee as e
                                             inner join Customer as c ON c.SupportRepId = e.EmployeeId
                                             inner join Invoice as i ON i.CustomerId = c.CustomerId";
@@ -72,7 +73,15 @@ namespace ChinookAPI.DataAccess
                     var agent = new SalesAgent()
                     {
                         AgentName = reader["Agent Name"].ToString(),
-                        InvoiceId = (int)reader["Invoice Id"]
+                        InvoiceId = (int)reader["InvoiceId"],
+                        CustomerId = (int)reader["CustomerId"],
+                        InvoiceDate = (DateTime)reader["InvoiceDate"],
+                        BillingAddress = reader["BillingAddress"].ToString(),
+                        BillingCity = reader["BillingCity"].ToString(),
+                        BillingState = reader["BillingState"].ToString(),
+                        BillingCountry = reader["BillingCountry"].ToString(),
+                        BillingPostalCode = reader["BillingPostalCode"].ToString(),
+                        Total = (decimal)reader["Total"]
                     };
                     _agents.Add(agent);
                 }
